efff98d6791c7eb06fbd0530d34cbbfd6cdbca02
Paulo Castro <59098978+phmcastro702@users.noreply.github.com>
2020-11-20 23:23:37 -0300

    finished memo game features with gif

--- main.js
+++ main.js
@@ -14,6 +14,12 @@ var cardsArray = [
     
 ];
 
+// duplicating the cardsArray using the spread operator
+let gameGrid = [...cardsArray, ...cardsArray];
+gameGrid.sort(() => {
+    return 0.5 - Math.random();
+});
+
 let game = document.getElementById('game-board');
 
 let grid = document.createElement('section');
@@ -22,11 +28,80 @@ grid.setAttribute('class', 'grid');
 
 game.appendChild(grid);
 
-for(let card of cardsArray)
+for(let card of gameGrid)
 {
     let tempCard = document.createElement('div');
     tempCard.classList.add('card');
     tempCard.dataset.name = card.name;
-    tempCard.style.backgroundImage = `url(${card.img})`;
+    var front = document.createElement('div');
+    front.classList.add('front');
+    var back = document.createElement('div');
+    back.classList.add('back');
+    back.style.backgroundImage = `url(${card.img})`;
+
     grid.appendChild(tempCard);
+    tempCard.appendChild(front);
+    tempCard.appendChild(back);
 }
+
+var match = () => {
+
+    [...document.querySelectorAll('.selected')].map((item) => item.classList.add('match'));
+
+};
+
+var resetGuesses = () => {
+    guesses[0] = '';
+    guesses[1] = '';
+    selectedCount = 0;
+    previousTarget = null;
+
+    [...document.querySelectorAll('.selected')].map((item) => item.classList.remove('selected'));
+};
+
+
+let guesses = ['', ''];
+let selectedCount = 0;
+let previousTarget = null;
+let delay = 1200;
+
+grid.addEventListener('click', (e) => {
+
+    let cardClicked = e.target;
+
+    if(cardClicked.nodeName === 'SECTION' || cardClicked === previousTarget || cardClicked.parentNode.classList.contains('match') || cardClicked.parentNode.classList.contains('selected')) 
+    { return; }
+
+    if (selectedCount < 2)
+    {
+        selectedCount++;
+
+        if(selectedCount === 1)
+        {
+            guesses[0] = cardClicked.parentNode.dataset.name;
+            cardClicked.parentNode.classList.add('selected');
+        }
+        else
+        {
+            guesses[1] = cardClicked.parentNode.dataset.name;
+            cardClicked.parentNode.classList.add('selected');
+        }
+
+        if(guesses[0] !== '' && guesses[1] !== '')
+        {
+            if(guesses[0] === guesses[1])
+            {
+                setTimeout(match, delay);
+                setTimeout(resetGuesses, delay);
+            }
+            else 
+            { 
+                setTimeout(resetGuesses, delay);
+                return;
+            }
+        }
+
+    }
+    
+    previousTarget = cardClicked;
+});
